1ff711c2d4de303bd8ef96c0f987468ee0a16207
David Wu <david@gofake1.net>
2015-09-28 20:03:31 -0400

    Initial commit

---
+++ ios/DiningBase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiningBase
+//
+//  Created by David Wu on 9/28/15.
+//  Copyright © 2015 Notre Dame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
