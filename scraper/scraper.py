ef1bca36d5000cf013c3272ed96a34f0e347b40b
David Wu <david@gofake1.net>
2015-11-02 21:39:55 -0500

    Work on scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,3 +1,36 @@
-#!/usr/bin/env python
-import BeautifulSoup
+# TODO: loop through all days
+# TODO: choose dining halls
+# TODO: loop through all categories in each day's menu
+# Right now the script only retrieves data for today's Legends' appetizers 
 
+from bs4 import BeautifulSoup
+import requests
+from requests.auth import HTTPBasicAuth
+
+url = 'http://nas.nd.edu'
+auth = HTTPBasicAuth('dwu4', 'rare_memes00')
+
+# Get main calendar
+calendar = requests.get(url, auth=auth)
+
+# Find forms for menus each day
+soup = BeautifulSoup(calendar.text, 'html.parser')
+menusPerDay = soup.find_all('form', id='viewMenu')
+
+# Get a menu
+someMenu = requests.post(url+'/menus.cfm', auth=auth)
+
+# Find nutrition data table
+soup = BeautifulSoup(someMenu.text, 'html.parser')
+nutritionDataTable = soup.find('form', {'name': 'menuAdd'}).find('table')
+
+# Get desired table rows
+nutritionData = nutritionDataTable.find_all('tr')
+
+# Display
+for row in nutritionData:
+    cells = row.find_all('td')
+    values = []
+    for cell in cells:
+        values.append(cell.get_text())
+    print values
