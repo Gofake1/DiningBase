9407f6f72bbdb2dc213fbf5df6078155d7e5052b
David Wu <david@gofake1.net>
2015-12-05 18:11:41 -0500

    Updated semantic-ui, added Jade

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ connection.connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'jade');
 
 router.get('/', function(req, res) {
     res.json({ message: 'GET successful, hello' });
@@ -29,9 +30,10 @@ router.route('/users')
             else
                 res.json({ message: 'success', results: results });
         });
+        console.log(query.sql);
     })
 
-    // Add new user
+    // Add a new user
     .post(function(req, res) {
         console.log(req.body);
         var user = {
@@ -50,7 +52,7 @@ router.route('/users')
         console.log(query.sql);
     });
 
-router.route('/users/:user_id')
+router.route('/users/:netid')
     // Get a user
     .get(function(req, res) {
         
@@ -63,7 +65,32 @@ router.route('/users/:user_id')
 router.route('/reviews')
     // Get all reviews
     .get(function(req, res) {
+        var query = connection.query('SELECT * FROM Review', function(err, results) {
+            if (err)
+                res.json({ error: err });
+            else
+                res.json({ message: 'success', results: results });
+        });
+    });
 
+router.route('/reviews/:name')
+    // Get all reviews for a food item
+    .get(function(req, res) {
+
+    })
+
+    // Update a review
+    .put()
+
+    // Add a new review
+    .post(function(req, res) {
+
+    });
+
+router.route('/today')
+    // Get today's menu
+    .get(function(req, res) {
+        res.render('today', { title: 'Today\'s Menu', message: 'Hello world' });
     });
 
 app.use('/api', router);
