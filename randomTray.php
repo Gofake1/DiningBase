709a2125bb92f3388b1a52fdecbe759ee1e869d8
David Wu <david@gofake1.net>
2015-12-16 20:56:56 -0500

    Added jquery

--- randomTray.php
+++ randomTray.php
@@ -9,6 +9,8 @@
   <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
   <link rel="stylesheet" type="text/css" href="main.css">
 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
   <style type="text/css">
   body {
     background-color: #FFFFFF;
